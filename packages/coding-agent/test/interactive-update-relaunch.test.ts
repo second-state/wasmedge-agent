Dmatut7/prime-agent

fix(coding-agent): replace TUI process after update (#1631)
* fix(coding-agent): replace TUI process after update

* fix(coding-agent): guard unsafe execve failures

* fix(coding-agent): restore the previous cwd when a thrown execve falls back

The relaunch chdirs before replacing the process; a thrown execve (Node >=26.1)
left the old process on the update cwd while the child fallback ran. The seam
now restores the captured previous cwd on the throw path so a failed attempt
leaves no silent process-state difference.

* docs(coding-agent): qualify the update-relaunch changelog with the platform restriction

---------

Co-authored-by: Sebastian <sebastian@primeintellect.ai>

packages/coding-agent/test/interactive-update-relaunch.test.ts
@@ -1,9 +1,29 @@
-import { describe, expect, it } from "vitest";
+import type * as ChildProcessModule from "child_process";
+import { describe, expect, it, vi } from "vitest";
+import type * as DaemonUpdateRestartModule from "../src/cli/daemon-update-restart.js";
+
+const updateMocks = vi.hoisted(() => ({
+	spawnSync: vi.fn(),
+	launchCoordinator: vi.fn(),
+}));
+
+vi.mock("child_process", async (importOriginal) => ({
+	...(await importOriginal<typeof ChildProcessModule>()),
+	spawnSync: updateMocks.spawnSync,
+}));
+
+vi.mock("../src/cli/daemon-update-restart.js", async (importOriginal) => ({
+	...(await importOriginal<typeof DaemonUpdateRestartModule>()),
+	launchDaemonUpdateRestartCoordinator: updateMocks.launchCoordinator,
+}));
+
 import { buildDaemonUpdateRestartReport } from "../src/cli/daemon-update-restart.js";
 import {
 	buildUpdateChildArgs,
 	buildUpdateRelaunchArgs,
+	InteractiveMode,
 	resolveInteractiveUpdateDaemonSocketPath,
+	tryExecUpdateRelaunch,
 	updateArgsIncludeSelf,
 } from "../src/modes/interactive/interactive-mode.js";
 
@@ -32,6 +52,209 @@ describe("buildUpdateRelaunchArgs", () => {
 			"/tmp/session.jsonl",
 		]);
 	});
+});
+
+describe("tryExecUpdateRelaunch", () => {
+	it("replaces the current process while preserving argv zero and the environment", () => {
+		const environment = { PRIME_AGENT_CODING_AGENT_DIR: "/tmp/agent", OMITTED: undefined };
+		const chdir = vi.fn();
+		const execve = vi.fn(() => undefined as never);
+
+		expect(
+			tryExecUpdateRelaunch(
+				{ command: "/usr/bin/node", args: ["--trace-warnings", "/opt/prime-agent/cli.js", "--resume", "session"] },
+				{
+					platform: "darwin",
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment,
+					chdir,
+					execve,
+				},
+			),
+		).toBe(true);
+		expect(chdir).toHaveBeenCalledWith("/tmp/project");
+		expect(execve).toHaveBeenCalledWith(
+			"/usr/bin/node",
+			["/usr/bin/node", "--trace-warnings", "/opt/prime-agent/cli.js", "--resume", "session"],
+			{ PRIME_AGENT_CODING_AGENT_DIR: "/tmp/agent" },
+		);
+
+		// A thrown execve restores the previous cwd before the fallback runs.
+		execve.mockImplementationOnce(() => {
+			throw new Error("execve failed");
+		});
+		expect(() =>
+			tryExecUpdateRelaunch(
+				{ command: "/usr/bin/node", args: ["cli.js"] },
+				{
+					platform: "darwin",
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment,
+					chdir,
+					execve,
+				},
+			),
+		).toThrow("execve failed");
+		expect(chdir).toHaveBeenLastCalledWith("/tmp/before");
+	});
+
+	it.each(["win32", "os400"])("keeps the compatible child relaunch on %s", (platform) => {
+		const chdir = vi.fn();
+		const execve = vi.fn(() => undefined as never);
+
+		expect(
+			tryExecUpdateRelaunch(
+				{ command: "node", args: ["cli.js"] },
+				{
+					platform,
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment: {},
+					chdir,
+					execve,
+				},
+			),
+		).toBe(false);
+		expect(chdir).not.toHaveBeenCalled();
+		expect(execve).not.toHaveBeenCalled();
+	});
+
+	it.each(["22.22.0", "24.13.0", "25.8.1", "26.0.0"])(
+		"keeps the compatible child relaunch when execve failures abort Node %s",
+		(nodeVersion) => {
+			const chdir = vi.fn();
+			const execve = vi.fn(() => undefined as never);
+
+			expect(
+				tryExecUpdateRelaunch(
+					{ command: "/usr/bin/node", args: ["cli.js"] },
+					{
+						platform: "linux",
+						nodeVersion,
+						cwd: "/tmp/project",
+						previousCwd: "/tmp/before",
+						environment: {},
+						chdir,
+						execve,
+					},
+				),
+			).toBe(false);
+			expect(chdir).not.toHaveBeenCalled();
+			expect(execve).not.toHaveBeenCalled();
+		},
+	);
+
+	it("keeps the compatible child relaunch when execve is unavailable", () => {
+		expect(
+			tryExecUpdateRelaunch(
+				{ command: "/usr/bin/node", args: ["cli.js"] },
+				{
+					platform: "linux",
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment: {},
+					chdir: vi.fn(),
+				},
+			),
+		).toBe(false);
+	});
+});
+
+describe("interactive self-update relaunch", () => {
+	it.skipIf(process.platform === "win32")(
+		"tears down and replaces the TUI process without waiting for a child TUI to quit",
+		async () => {
+			const events: string[] = [];
+			updateMocks.spawnSync.mockReset();
+			updateMocks.spawnSync.mockImplementation(() => {
+				events.push("update");
+				return { status: 0, signal: null } as never;
+			});
+			updateMocks.launchCoordinator.mockReset();
+			updateMocks.launchCoordinator.mockImplementation(async () => {
+				events.push("coordinator");
+				return {
+					version: 1,
+					requestId: "test-request",
+					socketPath: "/tmp/update.sock",
+					phase: "complete",
+					coordinator: { pid: process.pid },
+					counts: { total: 0, restored: 0, resumed: 0, failed: 0 },
+					failures: [],
+					startedAt: "2026-08-21T00:00:00.000Z",
+					updatedAt: "2026-08-21T00:00:01.000Z",
+				};
+			});
+
+			const updateProcess = process as NodeJS.Process & {
+				execve?: (file: string, args: string[], environment: NodeJS.ProcessEnv) => never;
+			};
+			const originalExecve = updateProcess.execve;
+			const originalNodeVersion = Object.getOwnPropertyDescriptor(process.versions, "node");
+			const execve = vi.fn((_file: string, _args: string[], _environment: NodeJS.ProcessEnv) => {
+				events.push("execve");
+				return undefined as never;
+			});
+			updateProcess.execve = execve;
+			Object.defineProperty(process.versions, "node", { ...originalNodeVersion, value: "26.1.0" });
+
+			const receiver = {
+				connectionState: {
+					activeSessionId: "active-session",
+					sessionFile: "/tmp/session.jsonl",
+				},
+				fullscreenEnabled: false,
+				options: {
+					daemonSocketPath: "/tmp/update.sock",
+					onShutdown: async () => events.push("shutdown"),
+				},
+				getCurrentCwd: () => process.cwd(),
+				stopWorkingLoader: () => events.push("loader-stop"),
+				stop: () => events.push("mode-stop"),
+				ui: {
+					terminal: { drainInput: async () => events.push("drain-input") },
+					stop: () => events.push("ui-stop"),
+				},
+				agentConnection: {
+					dispose: async () => events.push("connection-dispose"),
+				},
+			};
+			const handleUpdateCommand = (
+				InteractiveMode.prototype as unknown as {
+					handleUpdateCommand(this: typeof receiver, args: string): Promise<void>;
+				}
+			).handleUpdateCommand;
+
+			try {
+				await handleUpdateCommand.call(receiver, "");
+			} finally {
+				updateProcess.execve = originalExecve;
+				if (originalNodeVersion) {
+					Object.defineProperty(process.versions, "node", originalNodeVersion);
+				}
+			}
+
+			expect(events).toEqual([
+				"loader-stop",
+				"drain-input",
+				"ui-stop",
+				"update",
+				"mode-stop",
+				"connection-dispose",
+				"shutdown",
+				"coordinator",
+				"execve",
+			]);
+			expect(updateMocks.spawnSync).toHaveBeenCalledTimes(1);
+			expect(execve.mock.calls[0]?.[1]).toEqual(expect.arrayContaining(["--resume", "/tmp/session.jsonl"]));
+		},
+	);
 });
 
 describe("buildUpdateChildArgs", () => {

second-state/wasmedge-agent

(cherry picked from commit 083c68dc022e57d0b3103b92238813ad8940029a)

packages/coding-agent/test/interactive-update-relaunch.test.ts
@@ -1,9 +1,29 @@
-import { describe, expect, it } from "vitest";
+import type * as ChildProcessModule from "child_process";
+import { describe, expect, it, vi } from "vitest";
+import type * as DaemonUpdateRestartModule from "../src/cli/daemon-update-restart.js";
+
+const updateMocks = vi.hoisted(() => ({
+	spawnSync: vi.fn(),
+	launchCoordinator: vi.fn(),
+}));
+
+vi.mock("child_process", async (importOriginal) => ({
+	...(await importOriginal<typeof ChildProcessModule>()),
+	spawnSync: updateMocks.spawnSync,
+}));
+
+vi.mock("../src/cli/daemon-update-restart.js", async (importOriginal) => ({
+	...(await importOriginal<typeof DaemonUpdateRestartModule>()),
+	launchDaemonUpdateRestartCoordinator: updateMocks.launchCoordinator,
+}));
+
 import { buildDaemonUpdateRestartReport } from "../src/cli/daemon-update-restart.js";
 import {
 	buildUpdateChildArgs,
 	buildUpdateRelaunchArgs,
+	InteractiveMode,
 	resolveInteractiveUpdateDaemonSocketPath,
+	tryExecUpdateRelaunch,
 	updateArgsIncludeSelf,
 } from "../src/modes/interactive/interactive-mode.js";
 
@@ -32,6 +52,212 @@ describe("buildUpdateRelaunchArgs", () => {
 			"/tmp/session.jsonl",
 		]);
 	});
+});
+
+describe("tryExecUpdateRelaunch", () => {
+	it("replaces the current process while preserving argv zero and the environment", () => {
+		const environment = { WASMEDGE_AGENT_CODING_AGENT_DIR: "/tmp/agent", OMITTED: undefined };
+		const chdir = vi.fn();
+		const execve = vi.fn(() => undefined as never);
+
+		expect(
+			tryExecUpdateRelaunch(
+				{
+					command: "/usr/bin/node",
+					args: ["--trace-warnings", "/opt/wasmedge-agent/cli.js", "--resume", "session"],
+				},
+				{
+					platform: "darwin",
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment,
+					chdir,
+					execve,
+				},
+			),
+		).toBe(true);
+		expect(chdir).toHaveBeenCalledWith("/tmp/project");
+		expect(execve).toHaveBeenCalledWith(
+			"/usr/bin/node",
+			["/usr/bin/node", "--trace-warnings", "/opt/wasmedge-agent/cli.js", "--resume", "session"],
+			{ WASMEDGE_AGENT_CODING_AGENT_DIR: "/tmp/agent" },
+		);
+
+		// A thrown execve restores the previous cwd before the fallback runs.
+		execve.mockImplementationOnce(() => {
+			throw new Error("execve failed");
+		});
+		expect(() =>
+			tryExecUpdateRelaunch(
+				{ command: "/usr/bin/node", args: ["cli.js"] },
+				{
+					platform: "darwin",
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment,
+					chdir,
+					execve,
+				},
+			),
+		).toThrow("execve failed");
+		expect(chdir).toHaveBeenLastCalledWith("/tmp/before");
+	});
+
+	it.each(["win32", "os400"])("keeps the compatible child relaunch on %s", (platform) => {
+		const chdir = vi.fn();
+		const execve = vi.fn(() => undefined as never);
+
+		expect(
+			tryExecUpdateRelaunch(
+				{ command: "node", args: ["cli.js"] },
+				{
+					platform,
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment: {},
+					chdir,
+					execve,
+				},
+			),
+		).toBe(false);
+		expect(chdir).not.toHaveBeenCalled();
+		expect(execve).not.toHaveBeenCalled();
+	});
+
+	it.each(["22.22.0", "24.13.0", "25.8.1", "26.0.0"])(
+		"keeps the compatible child relaunch when execve failures abort Node %s",
+		(nodeVersion) => {
+			const chdir = vi.fn();
+			const execve = vi.fn(() => undefined as never);
+
+			expect(
+				tryExecUpdateRelaunch(
+					{ command: "/usr/bin/node", args: ["cli.js"] },
+					{
+						platform: "linux",
+						nodeVersion,
+						cwd: "/tmp/project",
+						previousCwd: "/tmp/before",
+						environment: {},
+						chdir,
+						execve,
+					},
+				),
+			).toBe(false);
+			expect(chdir).not.toHaveBeenCalled();
+			expect(execve).not.toHaveBeenCalled();
+		},
+	);
+
+	it("keeps the compatible child relaunch when execve is unavailable", () => {
+		expect(
+			tryExecUpdateRelaunch(
+				{ command: "/usr/bin/node", args: ["cli.js"] },
+				{
+					platform: "linux",
+					nodeVersion: "26.1.0",
+					cwd: "/tmp/project",
+					previousCwd: "/tmp/before",
+					environment: {},
+					chdir: vi.fn(),
+				},
+			),
+		).toBe(false);
+	});
+});
+
+describe("interactive self-update relaunch", () => {
+	it.skipIf(process.platform === "win32")(
+		"tears down and replaces the TUI process without waiting for a child TUI to quit",
+		async () => {
+			const events: string[] = [];
+			updateMocks.spawnSync.mockReset();
+			updateMocks.spawnSync.mockImplementation(() => {
+				events.push("update");
+				return { status: 0, signal: null } as never;
+			});
+			updateMocks.launchCoordinator.mockReset();
+			updateMocks.launchCoordinator.mockImplementation(async () => {
+				events.push("coordinator");
+				return {
+					version: 1,
+					requestId: "test-request",
+					socketPath: "/tmp/update.sock",
+					phase: "complete",
+					coordinator: { pid: process.pid },
+					counts: { total: 0, restored: 0, resumed: 0, failed: 0 },
+					failures: [],
+					startedAt: "2026-08-21T00:00:00.000Z",
+					updatedAt: "2026-08-21T00:00:01.000Z",
+				};
+			});
+
+			const updateProcess = process as NodeJS.Process & {
+				execve?: (file: string, args: string[], environment: NodeJS.ProcessEnv) => never;
+			};
+			const originalExecve = updateProcess.execve;
+			const originalNodeVersion = Object.getOwnPropertyDescriptor(process.versions, "node");
+			const execve = vi.fn((_file: string, _args: string[], _environment: NodeJS.ProcessEnv) => {
+				events.push("execve");
+				return undefined as never;
+			});
+			updateProcess.execve = execve;
+			Object.defineProperty(process.versions, "node", { ...originalNodeVersion, value: "26.1.0" });
+
+			const receiver = {
+				connectionState: {
+					activeSessionId: "active-session",
+					sessionFile: "/tmp/session.jsonl",
+				},
+				fullscreenEnabled: false,
+				options: {
+					daemonSocketPath: "/tmp/update.sock",
+					onShutdown: async () => events.push("shutdown"),
+				},
+				getCurrentCwd: () => process.cwd(),
+				stopWorkingLoader: () => events.push("loader-stop"),
+				stop: () => events.push("mode-stop"),
+				ui: {
+					terminal: { drainInput: async () => events.push("drain-input") },
+					stop: () => events.push("ui-stop"),
+				},
+				agentConnection: {
+					dispose: async () => events.push("connection-dispose"),
+				},
+			};
+			const handleUpdateCommand = (
+				InteractiveMode.prototype as unknown as {
+					handleUpdateCommand(this: typeof receiver, args: string): Promise<void>;
+				}
+			).handleUpdateCommand;
+
+			try {
+				await handleUpdateCommand.call(receiver, "");
+			} finally {
+				updateProcess.execve = originalExecve;
+				if (originalNodeVersion) {
+					Object.defineProperty(process.versions, "node", originalNodeVersion);
+				}
+			}
+
+			expect(events).toEqual([
+				"loader-stop",
+				"drain-input",
+				"ui-stop",
+				"update",
+				"mode-stop",
+				"connection-dispose",
+				"shutdown",
+				"coordinator",
+				"execve",
+			]);
+			expect(updateMocks.spawnSync).toHaveBeenCalledTimes(1);
+			expect(execve.mock.calls[0]?.[1]).toEqual(expect.arrayContaining(["--resume", "/tmp/session.jsonl"]));
+		},
+	);
 });
 
 describe("buildUpdateChildArgs", () => {