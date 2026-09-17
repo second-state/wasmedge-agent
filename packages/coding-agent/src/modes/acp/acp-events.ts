0niel/prime-agent

fix(acp): preserve assistant message boundaries (#1781)

packages/coding-agent/src/modes/acp/acp-events.ts
@@ -54,12 +54,12 @@ function textContent(text: string): { type: "text"; text: string } {
  * `thinking_delta`) and carries a plain string, so reasoning and visible answer
  * text are distinct ACP update kinds a client can render or hide separately.
  */
-function assistantDeltaUpdates(event: AssistantMessageEvent): AcpSessionUpdate[] {
+function assistantDeltaUpdates(event: AssistantMessageEvent, messageId: string): AcpSessionUpdate[] {
 	if (event.type === "thinking_delta" && event.delta.length > 0) {
-		return [{ sessionUpdate: "agent_thought_chunk", content: textContent(event.delta) }];
+		return [{ sessionUpdate: "agent_thought_chunk", messageId, content: textContent(event.delta) }];
 	}
 	if (event.type === "text_delta" && event.delta.length > 0) {
-		return [{ sessionUpdate: "agent_message_chunk", content: textContent(event.delta) }];
+		return [{ sessionUpdate: "agent_message_chunk", messageId, content: textContent(event.delta) }];
 	}
 	return [];
 }
@@ -121,15 +121,18 @@ function ipythonRichOutput(result: unknown): PrimeAgentIpythonMeta | undefined {
 	return meta.attachments || meta.diffCount !== undefined ? meta : undefined;
 }
 
-/**
- * Correlates streaming bash output with its run.
- *
- * `bash_output` carries no `runId`, so the id of the most recent `bash_start` is
- * tracked here. Output would otherwise be attributed to a bare fallback id that
- * no `bash_start` ever used, leaving the chunks unattached to any tool call.
- */
+/** Correlates streamed bash output and assistant chunks with their owning run or message. */
 export interface AcpEventMappingState {
 	activeBashRunId?: string;
+	activeAssistantMessageId?: string;
+	nextAssistantMessageSequence?: number;
+}
+
+function startAssistantMessage(state: AcpEventMappingState): string {
+	const sequence = (state.nextAssistantMessageSequence ?? 0) + 1;
+	state.nextAssistantMessageSequence = sequence;
+	state.activeAssistantMessageId = `prime-agent-assistant-${sequence}`;
+	return state.activeAssistantMessageId;
 }
 
 export function acpUpdatesForSessionEvent(
@@ -137,9 +140,20 @@ export function acpUpdatesForSessionEvent(
 	state: AcpEventMappingState = {},
 ): AcpSessionUpdate[] {
 	switch (event.type) {
+		case "message_start":
+			if (event.message.role === "assistant") startAssistantMessage(state);
+			return [];
+
 		case "message_update":
 			if (event.message.role !== "assistant") return [];
-			return assistantDeltaUpdates(event.assistantMessageEvent);
+			return assistantDeltaUpdates(
+				event.assistantMessageEvent,
+				state.activeAssistantMessageId ?? startAssistantMessage(state),
+			);
+
+		case "message_end":
+			if (event.message.role === "assistant") state.activeAssistantMessageId = undefined;
+			return [];
 
 		case "tool_execution_start": {
 			const cell = event.toolName === IPYTHON_TOOL_NAME ? ipythonCellSource(event.args) : undefined;

packages/coding-agent/test/acp-events.test.ts
@@ -20,12 +20,46 @@ function assistantDelta(type: "text_delta" | "thinking_delta", delta: string): A
 describe("ACP session event mapping", () => {
 	it("maps assistant text deltas to agent_message_chunk", () => {
 		const updates = acpUpdatesForSessionEvent(assistantDelta("text_delta", "hello"));
-		expect(updates).toEqual([{ sessionUpdate: "agent_message_chunk", content: { type: "text", text: "hello" } }]);
+		expect(updates).toEqual([
+			{
+				sessionUpdate: "agent_message_chunk",
+				messageId: "prime-agent-assistant-1",
+				content: { type: "text", text: "hello" },
+			},
+		]);
 	});
 
 	it("maps thinking deltas to agent_thought_chunk, not visible text", () => {
 		const updates = acpUpdatesForSessionEvent(assistantDelta("thinking_delta", "reasoning"));
-		expect(updates).toEqual([{ sessionUpdate: "agent_thought_chunk", content: { type: "text", text: "reasoning" } }]);
+		expect(updates).toEqual([
+			{
+				sessionUpdate: "agent_thought_chunk",
+				messageId: "prime-agent-assistant-1",
+				content: { type: "text", text: "reasoning" },
+			},
+		]);
+	});
+
+	it("assigns one message id per assistant message", () => {
+		const state: AcpEventMappingState = {};
+		const message = { role: "assistant", content: [], usage: {} } as never;
+		const start = { type: "message_start", message } as AgentConnectionSessionEvent;
+		const end = { type: "message_end", message } as AgentConnectionSessionEvent;
+
+		expect(acpUpdatesForSessionEvent(start, state)).toEqual([]);
+		expect(acpUpdatesForSessionEvent(assistantDelta("thinking_delta", "think"), state)[0]).toMatchObject({
+			messageId: "prime-agent-assistant-1",
+		});
+		expect(acpUpdatesForSessionEvent(assistantDelta("text_delta", "answer"), state)[0]).toMatchObject({
+			messageId: "prime-agent-assistant-1",
+		});
+		expect(acpUpdatesForSessionEvent(end, state)).toEqual([]);
+		expect(state.activeAssistantMessageId).toBeUndefined();
+
+		expect(acpUpdatesForSessionEvent(start, state)).toEqual([]);
+		expect(acpUpdatesForSessionEvent(assistantDelta("text_delta", "next"), state)[0]).toMatchObject({
+			messageId: "prime-agent-assistant-2",
+		});
 	});
 
 	it("ignores empty deltas and non-assistant messages", () => {

second-state/wasmedge-agent

(cherry picked from commit b5ee2f81a59510e7225a0db10d65102e91e98803)

packages/coding-agent/src/modes/acp/acp-events.ts
@@ -54,12 +54,12 @@ function textContent(text: string): { type: "text"; text: string } {
  * `thinking_delta`) and carries a plain string, so reasoning and visible answer
  * text are distinct ACP update kinds a client can render or hide separately.
  */
-function assistantDeltaUpdates(event: AssistantMessageEvent): AcpSessionUpdate[] {
+function assistantDeltaUpdates(event: AssistantMessageEvent, messageId: string): AcpSessionUpdate[] {
 	if (event.type === "thinking_delta" && event.delta.length > 0) {
-		return [{ sessionUpdate: "agent_thought_chunk", content: textContent(event.delta) }];
+		return [{ sessionUpdate: "agent_thought_chunk", messageId, content: textContent(event.delta) }];
 	}
 	if (event.type === "text_delta" && event.delta.length > 0) {
-		return [{ sessionUpdate: "agent_message_chunk", content: textContent(event.delta) }];
+		return [{ sessionUpdate: "agent_message_chunk", messageId, content: textContent(event.delta) }];
 	}
 	return [];
 }
@@ -121,15 +121,18 @@ function rustRichOutput(result: unknown): WasmEdgeAgentRustMeta | undefined {
 	return meta.attachments || meta.diffCount !== undefined ? meta : undefined;
 }
 
-/**
- * Correlates streaming bash output with its run.
- *
- * `bash_output` carries no `runId`, so the id of the most recent `bash_start` is
- * tracked here. Output would otherwise be attributed to a bare fallback id that
- * no `bash_start` ever used, leaving the chunks unattached to any tool call.
- */
+/** Correlates streamed bash output and assistant chunks with their owning run or message. */
 export interface AcpEventMappingState {
 	activeBashRunId?: string;
+	activeAssistantMessageId?: string;
+	nextAssistantMessageSequence?: number;
+}
+
+function startAssistantMessage(state: AcpEventMappingState): string {
+	const sequence = (state.nextAssistantMessageSequence ?? 0) + 1;
+	state.nextAssistantMessageSequence = sequence;
+	state.activeAssistantMessageId = `wasmedge-agent-assistant-${sequence}`;
+	return state.activeAssistantMessageId;
 }
 
 export function acpUpdatesForSessionEvent(
@@ -137,9 +140,20 @@ export function acpUpdatesForSessionEvent(
 	state: AcpEventMappingState = {},
 ): AcpSessionUpdate[] {
 	switch (event.type) {
+		case "message_start":
+			if (event.message.role === "assistant") startAssistantMessage(state);
+			return [];
+
 		case "message_update":
 			if (event.message.role !== "assistant") return [];
-			return assistantDeltaUpdates(event.assistantMessageEvent);
+			return assistantDeltaUpdates(
+				event.assistantMessageEvent,
+				state.activeAssistantMessageId ?? startAssistantMessage(state),
+			);
+
+		case "message_end":
+			if (event.message.role === "assistant") state.activeAssistantMessageId = undefined;
+			return [];
 
 		case "tool_execution_start": {
 			const cell = event.toolName === RUST_TOOL_NAME ? rustCellSource(event.args) : undefined;

packages/coding-agent/test/acp-events.test.ts
@@ -20,12 +20,46 @@ function assistantDelta(type: "text_delta" | "thinking_delta", delta: string): A
 describe("ACP session event mapping", () => {
 	it("maps assistant text deltas to agent_message_chunk", () => {
 		const updates = acpUpdatesForSessionEvent(assistantDelta("text_delta", "hello"));
-		expect(updates).toEqual([{ sessionUpdate: "agent_message_chunk", content: { type: "text", text: "hello" } }]);
+		expect(updates).toEqual([
+			{
+				sessionUpdate: "agent_message_chunk",
+				messageId: "wasmedge-agent-assistant-1",
+				content: { type: "text", text: "hello" },
+			},
+		]);
 	});
 
 	it("maps thinking deltas to agent_thought_chunk, not visible text", () => {
 		const updates = acpUpdatesForSessionEvent(assistantDelta("thinking_delta", "reasoning"));
-		expect(updates).toEqual([{ sessionUpdate: "agent_thought_chunk", content: { type: "text", text: "reasoning" } }]);
+		expect(updates).toEqual([
+			{
+				sessionUpdate: "agent_thought_chunk",
+				messageId: "wasmedge-agent-assistant-1",
+				content: { type: "text", text: "reasoning" },
+			},
+		]);
+	});
+
+	it("assigns one message id per assistant message", () => {
+		const state: AcpEventMappingState = {};
+		const message = { role: "assistant", content: [], usage: {} } as never;
+		const start = { type: "message_start", message } as AgentConnectionSessionEvent;
+		const end = { type: "message_end", message } as AgentConnectionSessionEvent;
+
+		expect(acpUpdatesForSessionEvent(start, state)).toEqual([]);
+		expect(acpUpdatesForSessionEvent(assistantDelta("thinking_delta", "think"), state)[0]).toMatchObject({
+			messageId: "wasmedge-agent-assistant-1",
+		});
+		expect(acpUpdatesForSessionEvent(assistantDelta("text_delta", "answer"), state)[0]).toMatchObject({
+			messageId: "wasmedge-agent-assistant-1",
+		});
+		expect(acpUpdatesForSessionEvent(end, state)).toEqual([]);
+		expect(state.activeAssistantMessageId).toBeUndefined();
+
+		expect(acpUpdatesForSessionEvent(start, state)).toEqual([]);
+		expect(acpUpdatesForSessionEvent(assistantDelta("text_delta", "next"), state)[0]).toMatchObject({
+			messageId: "wasmedge-agent-assistant-2",
+		});
 	});
 
 	it("ignores empty deltas and non-assistant messages", () => {